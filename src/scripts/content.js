82a4e043b78d15cd57a0140b95e3e2844e4c51e4
MarcBaeuerle <92479171+MarcBaeuerle@users.noreply.github.com>
2024-01-04 21:31:42 -0800

    friendship with vite ended. Now webpack is my best friend

--- src/scripts/content.js
+++
@@ -1,7 +0,0 @@
-export const test = () => {
-  window.alert(`hello`);
-}
-
-export function helloThere() {
-  window.alert(`hello`);
-}
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  build: {
-    // Avoid using hash in file names
-    chunkFileNames: 'chunks/[name].js',
-    assetFileNames: 'assets/[name].[ext]'
-  }
-};
